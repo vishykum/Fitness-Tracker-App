e125913acadff1ddf8383b7789129c570fd7a2bf
Vishal <vishykum@ymail.com>
2025-10-31 21:49:59 -0700

    Changed basename to Fitness-Tracker-App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -32,7 +32,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename='/fitness-tracker/'>
+    <BrowserRouter basename='/Fitness-Tracker-App/'>
     <div className="flex flex-col justify-start h-screen w-screen">
       <div className="sticky w-screen top-0">
         <Navbar darkMode={darkMode} setDarkMode={setDarkMode} isLoggedIn={username} setLoggedIn={setUsername}/>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss()],
-  base: '/fitness-tracker/'
+  base: '/Fitness-Tracker-App/'
 })
